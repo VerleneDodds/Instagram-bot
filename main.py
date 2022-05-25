1a586cb05dbbb00d812f0ded1a93974f171a175b
Verlene Dodds <kylan.dodds@gmail.com>
2022-05-25 17:55:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,15 @@ os.system('cls' if os.name == 'nt' else 'clear')
 # Message Followers
 # Auto login
 
-insta = InstaDM(username='verlenedodds', password='BashNShell!!$LinuxVarechO@!', headless=False)
+insta = InstaDM(username='verlenedodds', password='BashNShell!!$LinuxVarechO@!', headless=True)
 
+x = 1
 
-for user in follow_list:	
+for user in follow_list:
+    if x<= 191:
+        print("Finished already.")
+        x = x + 1
     # Send message
-    insta.sendMessage(user, message='This is a message to verify that the bot works!')
+    else:
+        insta.sendMessage(user, message='Hello, my name is Verlene, I am sorry to bug you, but my friend Quinn has dissapered from social medias, and I have not been able to reach out to her, and it is important that I talk to her, due to an event with another person, if you know Quinn Carter, and have a way to contact her, please let me know, it would be extremely helpful and appreciated')
 	
-# Send message
-#insta.sendGroupMessage(users=['user1', 'user2'], message='Hey !')
